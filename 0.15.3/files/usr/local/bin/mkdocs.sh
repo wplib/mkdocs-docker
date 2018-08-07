c5eb9dfc80b5e0c31a7396d201632a6b34a37fc8
MickMake <github@mickmake.com>
2018-08-07 15:51:13 +1000

    Added interactive

--- 0.15.3/files/usr/local/bin/mkdocs.sh
+++ 0.15.3/files/usr/local/bin/mkdocs.sh
@@ -1,21 +1,28 @@
 #!/bin/sh
 
-ARG1=$1
-shift
-case $ARG1 in
-	'build'|'gh-deploy'|'new')
-		ARGS=""
-		;;
+if [ "$1" == "interactive" ]
+then
+	shift
+	ARG1=$1
+	shift
+	case $ARG1 in
+		'build'|'gh-deploy'|'new')
+			ARGS=""
+			;;
 
-	'serve')
-		ARGS="-a 0.0.0.0:8000"
-		;;
+		'serve')
+			echo "WPLib Box: Warning - Can only serve from container."
+			exit
+			;;
 
-	*)
-		ARG1="serve"
-		ARGS="-a 0.0.0.0:8000"
-		;;
-esac
+		*)
+			ARGS=""
+			;;
+	esac
+else
+	ARG1="serve"
+	ARGS="-a 0.0.0.0:8000"
+fi
 
-echo /usr/local/bin/mkdocs $ARG1 $ARGS "$@"
+# echo /usr/local/bin/mkdocs $ARG1 $ARGS "$@"
 exec /usr/local/bin/mkdocs $ARG1 $ARGS "$@"
